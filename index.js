e9536ebc432f981b9e26153c2207543be8286418
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-25 17:10:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 // Show the button when scrolling down 100px
 window.onscroll = function () {
-  const scrollToTopButton = document.querySelector(".scroll-to-top");
-  if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
-  ) {
-    scrollToTopButton.classList.add("show");
-  } else {
-    scrollToTopButton.classList.remove("show");
-  }
+    const scrollToTopButton = document.querySelector(".scroll-to-top");
+    if (
+        document.body.scrollTop > 100 ||
+        document.documentElement.scrollTop > 100
+    ) {
+        scrollToTopButton.classList.add("show");
+    } else {
+        scrollToTopButton.classList.remove("show");
+    }
 };
 
 // Smooth scroll to the top when the button is clicked
 document.querySelector(".scroll-to-top").onclick = function (e) {
-  e.preventDefault();
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
 };
