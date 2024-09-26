94426013a2f70090957aee098e4e38e34f775638
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-26 14:20:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+// Scroll to Top Button
 const scrollToTopBtn = document.querySelector('.scroll-to-top');
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 200) {
+    if (window.scrollY > 300) {
         scrollToTopBtn.style.display = 'block';
     } else {
         scrollToTopBtn.style.display = 'none';
     }
 });
 
-scrollToTopBtn.addEventListener('click', (event) => {
-    event.preventDefault();
+scrollToTopBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
