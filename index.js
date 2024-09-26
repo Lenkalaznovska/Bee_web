d892d92cde3263c8c4f4ad4928d11cd6ef0ab6cc
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-26 16:26:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
-// index.js
-document.addEventListener("DOMContentLoaded", function() {
-    const cookieConsent = document.getElementById("cookieConsent");
-    const acceptCookies = document.getElementById("acceptCookies");
-    const scrollToTopButton = document.querySelector('.scroll-to-top');
+// Smooth scroll to top functionality
+const scrollToTopButton = document.querySelector('.scroll-to-top');
 
-    // Zkontroluj, zda byly cookies již přijaty
-    if (!localStorage.getItem("cookiesAccepted")) {
-        cookieConsent.style.display = "block"; // Zobraz banner
-    }
-
-    // Přijmout cookies
-    acceptCookies.addEventListener("click", function() {
-        localStorage.setItem("cookiesAccepted", "true");
-        cookieConsent.style.display = "none"; // Skryj banner
-    });
-
-    // Zobrazit tlačítko "Scroll to Top" při skrolování
-    window.addEventListener("scroll", function() {
-        if (window.scrollY > 300) {
-            scrollToTopButton.style.display = "block"; // Zobraz tlačítko
-        } else {
-            scrollToTopButton.style.display = "none"; // Skryj tlačítko
-        }
-    });
-
-    // Přidat funkci skrolování na tlačítko
-    scrollToTopButton.addEventListener('click', function() {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
+scrollToTopButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
     });
 });
