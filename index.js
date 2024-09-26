93024e032b0a38dedf81d9f66e592a3e5bad23c6
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-26 14:18:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-// JavaScript pro scrollování nahoru
 const scrollToTopBtn = document.querySelector('.scroll-to-top');
-
-scrollToTopBtn.addEventListener('click', () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-    });
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = 'block';
+    } else {
+        scrollToTopBtn.style.display = 'none';
+    }
 });
 
-// Animace při scrollování
-const textContainers = document.querySelectorAll('.text-container');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('visible');
-            observer.unobserve(entry.target);
-        }
-    });
-}, { threshold: 0.1 });
-
-textContainers.forEach(container => {
-    observer.observe(container);
+scrollToTopBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 });
