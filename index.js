1b0a41a4afeb3caba33493ebc2c7e14fa0cbab6f
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-27 14:18:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Smooth Scroll to Top
+ // Smooth Scroll to Top
 const scrollToTopButton = document.querySelector('.scroll-to-top');
 scrollToTopButton.addEventListener('click', () => {
     window.scrollTo({
@@ -14,20 +14,4 @@ window.addEventListener('scroll', () => {
     } else {
         scrollToTopButton.style.display = 'none';
     }
-});
-
-// Zobrazení cookies lišty, pokud souhlas nebyl udělen
-window.addEventListener('load', () => {
-    const cookieConsent = document.getElementById('cookieConsent');
-    const acceptCookies = document.getElementById('acceptCookies');
-    const cookiesAccepted = localStorage.getItem('cookiesAccepted');
-
-    if (!cookiesAccepted) {
-        cookieConsent.style.display = 'block';
-    }
-
-    acceptCookies.addEventListener('click', () => {
-        localStorage.setItem('cookiesAccepted', 'true');
-        cookieConsent.style.display = 'none';
-    });
 });
