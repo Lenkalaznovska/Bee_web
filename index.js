018a3e4825f8b2c2e74b8de503cac9b8a15a7708
Lenka Lážnovská <159648392+Lenkalaznovska@users.noreply.github.com>
2024-09-27 14:30:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Skrýt tlačítko při načtení stránky
+document.getElementById("scrollToTop").style.display = "none";
+
 window.onscroll = function() {
     const scrollToTopButton = document.getElementById("scrollToTop");
+    // Kontrola, zda je uživatel posunut více než 100px od vrchu
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         scrollToTopButton.style.display = "block"; // Zobrazit tlačítko
     } else {
-        scrollToTopButton.style.display = "none"; // Skrýt tlačítko
+        scrollToTopButton.style.display = "none"; // Skrýt tlačítko, pokud je na začátku
     }
 };
 
